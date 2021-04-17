e3435b6aa6563682767c85f772935164fd6203d6
menaaziz27 <menaaziz27@gmail.com>
2021-04-17 14:16:12 +0200

    multiple pointer

---
+++ problem solving pattern/multiple pointers/sumZero.js
@@ -0,0 +1,19 @@
+function sumZero(arr) {
+	let left = 0;
+	let right = arr.length - 1;
+
+	while (left < right) {
+		sum = arr[left] + arr[right];
+		if (sum === 0) {
+			return [arr[left], arr[right]];
+		} else if (sum > 0) {
+			right--;
+		} else {
+			left++;
+		}
+	}
+}
+
+let sortedArray = [-3, -2, -1, 0, 2, 4, 6];
+
+console.log(sumZero(sortedArray));
