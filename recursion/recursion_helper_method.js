56656e13e2e88e14943c0aee763fe57ceee67441
menaaziz27 <menaaziz27@gmail.com>
2021-04-23 21:39:06 +0200

    recursion helper methods

---
+++ recursion/recursion_helper_method.js
@@ -0,0 +1,19 @@
+function collectOddValues(arr) {
+    let result = [];
+
+    function helper_input(input) {
+        if (input.length === 0) return;
+
+        if (input[0] % 2 !== 0) {
+            result.push(input[0]);
+        }
+
+        return helper_input(input.slice(1));
+    }
+
+    helper_input(arr);
+
+    return result;
+}
+
+console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
