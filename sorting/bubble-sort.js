58bd43cc80b3a74a0f167d1f37f6de6646331404
menaaziz27 <menaaziz27@gmail.com>
2021-05-03 08:24:26 +0200

    bubble sort optimized

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -1,10 +1,14 @@
 function bubbleSort(arr) {
+	let noSwaps;
 	for (let i = arr.length; i > 0; i--) {
+		noSwaps = true;
 		for (let j = 0; j < i; j++) {
 			if (arr[j] > arr[j + 1]) {
 				swap(arr, j, j + 1);
+				noSwaps = false;
 			}
 		}
+		if (noSwaps) break;
 	}
 	return arr;
 }
